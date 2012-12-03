e9274dc91908fa27278da68bdb0774204e45bda8
tko <tko@muba.jitu.org>
2012-12-03 16:35:49 +0900

    clean up.

--- src/org/jitu/wagtail/MainActivity.java
+++ src/org/jitu/wagtail/MainActivity.java
@@ -3,7 +3,6 @@ package org.jitu.wagtail;
 import java.io.File;
 import android.media.AudioManager;
 import android.os.Bundle;
-import android.os.Environment;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
