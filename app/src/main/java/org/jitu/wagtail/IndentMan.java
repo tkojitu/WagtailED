4ade82e0d4517db6bf6e6105a760310c7dfa1755
tko <tko@muba.jitu.org>
2014-12-24 22:16:34 +0900

    multi-line tabify.

--- app/src/main/java/org/jitu/wagtail/IndentMan.java
+++ app/src/main/java/org/jitu/wagtail/IndentMan.java
@@ -3,6 +3,7 @@ package org.jitu.wagtail;
 import android.support.annotation.NonNull;
 import android.text.Editable;
 import android.text.Selection;
+import android.text.Spanned;
 import android.text.TextWatcher;
 import android.text.method.TextKeyListener;
 import android.view.KeyEvent;
@@ -61,8 +62,29 @@ public class IndentMan extends TextKeyListener implements TextWatcher {
     }
 
     public void tabify(Editable s) {
-        int bos = findBeginningOfLine(s);
-        s.insert(bos, "  ");
+        int st = Selection.getSelectionStart(s);
+        int ed = Selection.getSelectionEnd(s);
+        s.setSpan(this, st, ed, Spanned.SPAN_MARK_POINT);
+        try {
+            Selection.setSelection(s, ed);
+            for (;;) {
+                int pos = moveBeginningOfLine(s);
+                s.insert(pos, "  ");
+                pos = moveBeginningOfLine(s);
+                int mark = s.getSpanStart(this);
+                if (pos <= mark) {
+                    break;
+                }
+                if (pos == backwardChar(s)) {
+                    break;
+                }
+            }
+            st = s.getSpanStart(this);
+            ed = s.getSpanEnd(this);
+            Selection.setSelection(s, st, ed);
+        } finally {
+            s.removeSpan(this);
+        }
     }
 
     private int findBeginningOfLine(Editable s) {
@@ -76,6 +98,31 @@ public class IndentMan extends TextKeyListener implements TextWatcher {
             }
         }
         return 0;
+    }
+
+    private int moveBeginningOfLine(Editable s) {
+        int pos = Selection.getSelectionStart(s);
+        if (pos == 0) {
+            Selection.setSelection(s, 0);
+            return 0;
+        }
+        for (; pos > 0; --pos) {
+            if (s.charAt(pos - 1) == '\n') {
+                Selection.setSelection(s, pos);
+                return pos;
+            }
+        }
+        Selection.setSelection(s, 0);
+        return 0;
+    }
+
+    private int backwardChar(Editable s) {
+        int pos = Selection.getSelectionStart(s);
+        if (pos == 0) {
+            return 0;
+        }
+        Selection.setSelection(s, pos - 1);
+        return pos - 1;
     }
 
     public void untabify(Editable s) {
